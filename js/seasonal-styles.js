6c9ff38a601205ca9b47210a9403f493d73b9013
Bill Newman <william.newman@seattlecolleges.edu>
2022-02-01 02:03:52 -0800

    Adds images, initial JS

---
+++ js/seasonal-styles.js
@@ -0,0 +1,7 @@
+$("document").ready(function(){
+    $('.seasons a').click(function(e){//find all a tags inside class of seasons
+        e.preventDefault();//stop default submission
+        var season = $(this).attr("href");//contents of href attribute of this element
+        alert(season);
+    });
+});
